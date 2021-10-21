ddfc63a14973dd839aaa5078de7959a474de0105
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-10-21 15:32:56 +0200

    fix(state): Add verbose by throwing if payload type is not expected

--- src/coolearning/utils/set-state.ts
+++ src/coolearning/utils/set-state.ts
@@ -5,23 +5,20 @@ export function setState (action, payload) {
     const state = window[STATE_ID]
     switch (action) {
         case State.IsLearning:
-            if (typeof payload === 'boolean') {
-                state[State.IsLearning] = payload
-            }
+            if (!(typeof payload === 'boolean')) throw new Error (`${State.IsLearning} is not a boolean`)
+            state[State.IsLearning] = payload
             break
         case State.LearningParameter:
-            if (
+            if (!(
                 typeof payload === 'string'
                 || payload === null
-            ) {
-                state[State.LearningParameter] = payload
-            }
+            )) throw new Error (`${State.LearningParameter} is nor a string, nor null`)
+            state[State.LearningParameter] = payload
+
             break
         case State.Devices:
-            state[State.Devices] = [
-                ...state[State.Devices],
-                payload,
-            ]
+            if (!(Array.isArray (payload))) throw new Error (`${State.Devices} is not an array`)
+            state[State.Devices] = payload
             break
         default:
             throw new Error ('action not found')
