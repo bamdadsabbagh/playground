e6e38c0475b62d8519b804d596b80f7347f12118
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-10-22 10:23:16 +0200

    fix(get-local-storage): Circular call to `initializeState`, early return is enough

--- src/coolearning/utils/get-local-storage.ts
+++ src/coolearning/utils/get-local-storage.ts
@@ -1,9 +1,8 @@
 import { STATE_ID } from '../constants'
-import { initializeState } from './initialize-state'
 
-export function getStorage () {
+export function getLocalStorage () {
     if (!window.localStorage) throw new Error ('localStorage not defined')
-    if (!window.localStorage[STATE_ID]) initializeState ()
+    if (!window.localStorage[STATE_ID]) return
 
     let json
 
