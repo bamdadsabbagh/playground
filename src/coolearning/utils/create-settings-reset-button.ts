79101b81c61cd8862346d85b6e6fda1b3f7040d3
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-10-22 10:24:34 +0200

    feat(settings): Add new `reset` button

---
+++ src/coolearning/utils/create-settings-reset-button.ts
@@ -0,0 +1,23 @@
+import { setState } from './set-state'
+import { StateExtended } from '../enums'
+
+/**
+ * @description create button on main interface to reset settings state
+ * @todo merge duplicate code with action button
+ */
+export function createSettingsResetButton () {
+    const button = document.createElement ('button')
+
+    button.style.display = 'block'
+    button.style.position = 'fixed'
+    button.style.top = '5px'
+    button.style.left = '72px'
+    button.innerText = 'reset'
+
+    button.addEventListener ('click', () => {
+        // alert ('work in progress')
+        setState (StateExtended.Reset)
+    })
+
+    document.body.insertBefore (button, document.body.firstChild.nextSibling)
+}
--- src/coolearning/utils/initialize-settings.ts
+++ src/coolearning/utils/initialize-settings.ts
@@ -1,10 +1,13 @@
 import { createSettingsShowButton } from './create-settings-show-button'
 import { createSettings } from './create-settings'
 import { initializeSettingsContent } from './initialize-settings-content'
+import { createSettingsResetButton } from './create-settings-reset-button'
 
 export function initializeSettings () {
 
     createSettingsShowButton ()
+
+    createSettingsResetButton ()
 
     createSettings ()
 
