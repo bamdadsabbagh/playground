5736a811ad4b0ec74467cdea32bd68b975bb79d2
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-10-22 10:25:26 +0200

    fix(state): Simplify state initialization by removing duplicates assignments

--- src/coolearning/utils/initialize-state.ts
+++ src/coolearning/utils/initialize-state.ts
@@ -1,26 +1,18 @@
 import { STATE, STATE_ID } from '../constants'
-import { setStorage } from './set-storage'
-import { getStorage } from './get-storage'
-import { State } from '../enums'
+import { setLocalStorage } from './set-local-storage'
+import { getLocalStorage } from './get-local-storage'
 
 export function initializeState () {
     if (window[STATE_ID]) return
 
     // storage?
-    const storage = getStorage ()
+    const storage = getLocalStorage ()
     if (storage) return window[STATE_ID] = storage
 
     // very first initialization
-    const state = {}
-    state[State.IsLearning] = STATE.isLearning
-    state[State.LearningParameter] = STATE.learningParameter
-    state[State.Devices] = STATE.devices
-    state[State.ControlByParameter] = STATE.controlByParameter
-    state[State.ParametersByControl] = STATE.parametersByControl
+    window[STATE_ID] = STATE
 
-    window[STATE_ID] = state
+    setLocalStorage (STATE)
 
-    setStorage (state)
-
-    return state
+    return STATE
 }
