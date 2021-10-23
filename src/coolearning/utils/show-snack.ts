3130e6eccdbdfde7523bcf892e81321f06707865
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-10-23 12:45:32 +0200

    feat(UI/snackbar): Add `showSnack()` utility function

---
+++ src/coolearning/utils/show-snack.ts
@@ -0,0 +1,40 @@
+import { SNACKBAR_ID, TIMEOUT } from '../constants'
+
+type ShowSnackbarProps = {
+    message: string,
+    actionHandler?: (e: Event) => void,
+    actionText?: string,
+    timeout?: number,
+}
+
+export function showSnack (
+    {
+        message,
+        actionHandler,
+        actionText,
+        timeout = 1000,
+    }: ShowSnackbarProps,
+): void {
+
+    if (!message) throw new Error ('message is not defined')
+    if (typeof message !== 'string') throw new Error ('message is not a string')
+
+    const notification = document.getElementById (SNACKBAR_ID)
+    const hasAction = actionHandler && actionText
+    const options = {
+        message,
+        actionHandler: hasAction ? actionHandler : undefined,
+        actionText: hasAction ? actionText : undefined,
+        timeout,
+    }
+
+    // if material JS not yet loaded, then recurse
+    // @ts-ignore
+    if (typeof notification.MaterialSnackbar === 'undefined') {
+        setTimeout (() => showSnack (options), TIMEOUT)
+        return
+    }
+
+    // @ts-ignore
+    notification.MaterialSnackbar.showSnackbar (options)
+}
