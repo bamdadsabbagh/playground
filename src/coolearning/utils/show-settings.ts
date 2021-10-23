c7bdf084f4a16a791042905f67cedec81eec2503
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-10-23 23:22:26 +0200

    feat(showSettings): Add future code to show new modal from dialog UI

--- src/coolearning/utils/show-settings.ts
+++ src/coolearning/utils/show-settings.ts
@@ -1,9 +1,14 @@
 import { getSettingsContainer } from './get-settings-container'
 
+// import { getSettingsDialogElement } from './get-settings-dialog-element'
+
 /**
  * @description show settings UI
+ * @todo replace with future dialog
  */
 export function showSettings (): void {
     const container = getSettingsContainer ()
     container.style.display = 'block'
+    // const dialog = getSettingsDialogElement ()
+    // dialog.showModal ()
 }
