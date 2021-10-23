5fe2880999aba9ad834c26da4890dd40004c18df
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-10-23 15:22:48 +0200

    feat(highlight-mode): Intercept click (draft, WIP)

--- src/coolearning/utils/handle-highlight-mode.ts
+++ src/coolearning/utils/handle-highlight-mode.ts
@@ -4,6 +4,16 @@ import { PARAMETERS } from '../constants'
  * @description highlight mode handler
  */
 export function handleHighlightMode (enable = true): void {
+
+    // intercept click
+    const onGlobalClick = (e) => {
+        // todo might need to set an HTML attribute to all parameter targets
+        console.log ('click intercepted', e.target)
+    }
+
+    document.onclick = enable ? onGlobalClick : undefined
+
+    // change background
     const background = enable ? 'red' : null
 
     Object.keys (PARAMETERS).forEach (index => {
