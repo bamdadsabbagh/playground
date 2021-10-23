dad295a58dc501baf59ec34f2ca03a7d2d76a0b1
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-10-23 16:03:00 +0200

    feat(highlight-mode): WIP intercept click

--- src/coolearning/utils/handle-highlight-mode.ts
+++ src/coolearning/utils/handle-highlight-mode.ts
@@ -1,23 +1,37 @@
 import { PARAMETERS } from '../constants'
+import { getDeepestFirstChild } from './get-deepest-first-child'
 
 /**
  * @description highlight mode handler
  */
 export function handleHighlightMode (enable = true): void {
 
+    // global pointer style
+    document.body.style.cursor = enable ? 'crosshair' : 'auto'
+
     // intercept click
-    const onGlobalClick = (e) => {
+    const onGlobalClick = function (e) {
         // todo might need to set an HTML attribute to all parameter targets
-        console.log ('click intercepted', e.target)
+        e.stopPropagation ()
+        e.preventDefault ()
+        e.stopImmediatePropagation ()
+        console.log ('click intercepted', e, e.target, e.currentTarget)
+        const targetElement = getDeepestFirstChild (e.target)
+        console.log (targetElement)
+        const closestElement = targetElement.closest ('#learningRate, #reset-button')
+        console.log (closestElement)
     }
 
     document.onclick = enable ? onGlobalClick : undefined
 
-    // change background
-    const background = enable ? 'red' : null
-
+    // change parameters
     Object.keys (PARAMETERS).forEach (index => {
         const parameter = PARAMETERS[index]
-        parameter.style.background = background
+
+        // pointer events
+        parameter.style.pointerEvents = enable ? 'none' : 'auto'
+
+        // background
+        parameter.style.background = enable ? 'red' : null
     })
 }
