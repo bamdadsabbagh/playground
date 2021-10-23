3af7a274c605b2c3eb1fadb97b5f3dfffbb01924
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-10-23 23:22:05 +0200

    feat(UI): Add dialog UI element initialization

--- src/coolearning/utils/initialize-settings.ts
+++ src/coolearning/utils/initialize-settings.ts
@@ -3,6 +3,7 @@ import { createSettingsContainer } from './create-settings-container'
 import { createSettingsContent } from './create-settings-content'
 import { createSettingsResetButton } from './create-settings-reset-button'
 import { createSettingsSnackbar } from './create-settings-snackbar'
+import { createSettingsDialog } from './create-settings-dialog'
 
 /**
  * @description initialize settings UI
@@ -21,4 +22,5 @@ export function initializeSettings (): void {
 
     createSettingsContent ()
 
+    createSettingsDialog ()
 }
