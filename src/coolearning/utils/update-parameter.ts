204aa34c239b6eb0960b24b7d840acb71f784797
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-10-27 07:11:43 +0200

    fix(update-parameter): Remove obsolete and repetitive `type` argument

--- src/coolearning/utils/update-parameter.ts
+++ src/coolearning/utils/update-parameter.ts
@@ -1,13 +1,11 @@
 import { getParameterElement } from './get-parameter-element'
 import { rangeMap } from './range-map'
-import { MIDITypes } from '../enums'
 import { isTabActive } from './is-tab-active'
 import { Parameter } from '../types'
 
 type UpdateParameterProps = {
     parameter: Parameter,
     value: number,
-    type: MIDITypes,
 }
 
 /**
@@ -17,57 +15,58 @@ export function updateParameter (
     {
         parameter,
         value,
-        type,
     }: UpdateParameterProps,
 ): void {
+
     const element = getParameterElement ({parameter})
 
     if (!isTabActive ()) return
 
     switch (element.tagName) {
 
-        case 'SELECT':
-            if (type === MIDITypes.Range) {
-                const length = element.children.length - 1
-                const v = rangeMap (value, 0, 127, 0, length)
-                const n = parseInt (v.toString ())
-                const areDifferent = n !== element.selectedIndex
+        case 'SELECT': {
+            const length = element.children.length - 1
+            const v = rangeMap (value, 0, 127, 0, length)
+            const n = parseInt (v.toString ())
+            const areDifferent = n !== element.selectedIndex
 
-                if (areDifferent) {
-                    element.selectedIndex = n
-                    element.dispatchEvent (new Event ('change'))
-                }
+            if (areDifferent) {
+                element.selectedIndex = n
+                element.dispatchEvent (new Event ('change'))
             }
-            break
 
-        case 'BUTTON':
+            break
+        }
+
+        case 'BUTTON': {
             if (value === 0) return
             element.click ()
             break
+        }
 
-        case 'INPUT':
-            if (type === MIDITypes.Range) {
-                const min = parseInt (element.min)
-                const max = parseInt (element.max)
-                const step = parseInt (element.step)
+        case 'INPUT': {
+            const min = parseInt (element.min)
+            const max = parseInt (element.max)
+            const step = parseInt (element.step)
 
-                const v = rangeMap (value, 0, 127, min, max)
-                const n = parseInt (v.toString ())
-                const isStep = (n % step) === 0
-                const areDifferent = n !== parseInt (element.value)
+            const v = rangeMap (value, 0, 127, min, max)
+            const n = parseInt (v.toString ())
+            const isStep = (n % step) === 0
+            const areDifferent = n !== parseInt (element.value)
 
-                if (isStep && areDifferent) {
-                    element.value = n.toString ()
-                    element.dispatchEvent (new Event ('change'))
-                }
+            if (isStep && areDifferent) {
+                element.value = n.toString ()
+                element.dispatchEvent (new Event ('change'))
             }
-            break
 
-        case 'LABEL':
-            if (type === MIDITypes.ButtonOn) {
-                element.click ()
-            }
             break
+        }
+
+        case 'LABEL': {
+            if (value === 0) return
+            element.click ()
+            break
+        }
 
         default:
             throw new Error (`${element.tagName} target not handled`)
