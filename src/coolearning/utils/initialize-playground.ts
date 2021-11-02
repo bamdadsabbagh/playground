99c53b96d565f30dacf7749c6ac9734ab2328158
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-11-02 09:29:51 +0100

    perf(playground): remove force playground settings scripts (replaced by higher order statements)

--- src/coolearning/utils/initialize-playground.ts
+++ src/coolearning/utils/initialize-playground.ts
@@ -1,14 +1,8 @@
 import { addMaterialScript } from './add-material-script'
-import { forcePlaygroundNetwork } from './force-playground-network'
-import { forcePlaygroundFeatures } from './force-playground-features'
 
 /**
  * @description adapt playground interface
  */
 export function initializePlayground () {
     addMaterialScript ()
-
-    forcePlaygroundNetwork ()
-
-    forcePlaygroundFeatures ()
 }
