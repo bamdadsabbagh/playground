0579665aedf7ab88a713653178fb7cd354031de1
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-11-08 13:11:23 +0100

    perf(linting): ignore typescript `any` for the time being

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: '@bamdadsabbagh/eslint-config',
   rules: {
     'init-declarations': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/member-delimiter-style': ['error', {
       'multiline': {
         'delimiter': 'semi',
