84b02eb15e9800f106b57018e51d714c76374847
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-11-08 13:10:08 +0100

    perf(quality): add eslint lib and configuration

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: '@bamdadsabbagh/eslint-config',
+  rules: {
+    'init-declarations': 'off',
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      'multiline': {
+        'delimiter': 'semi',
+        'requireLast': true,
+      },
+      'singleline': {
+        'delimiter': 'semi',
+        'requireLast': true,
+      },
+      'multilineDetection': 'brackets',
+    }],
+  },
+};
